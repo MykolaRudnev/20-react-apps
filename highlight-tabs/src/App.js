b50315873359069700106f5d709e122dec02acd8
Mykola Rudnev <RudnevMykola@gmail.com>
2020-07-17 19:50:51 +0200

    Refactoring code

--- highlight-tabs/src/App.js
+++ highlight-tabs/src/App.js
@@ -1,55 +1,20 @@
 import React from "react";
 
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  NavLink,
+  BrowserRouter as Router
 } from "react-router-dom";
 import "./App.css";
-import Tab from "./components/Tab";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Features from "./pages/Features";
+import Routes from './Routes'
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
       <div className="app">
         <div className="browser">
-          <div className="tabs">
-            <Tab>
-              <NavLink to="/" activeClassName="is-active" exact={true}>
-                {" "}
-                Home{" "}
-              </NavLink>
-            </Tab>
-            <Tab>
-              <NavLink to="/about" activeClassName="is-active">
-                {" "}
-                About{" "}
-              </NavLink>
-            </Tab>
-            <Tab>
-              <NavLink to="/features" activeClassName="is-active">
-                {" "}
-                Features{" "}
-              </NavLink>
-            </Tab>
-          </div>
+          <Header />
           <div className="viewport">
-            <Switch>
-              <Route path="/about">
-                <About />
-              </Route>
-              <Route path="/features">
-                <Features />
-              </Route>
-
-              <Route path="/" exact={true}>
-                <Home />
-              </Route>
-            </Switch>
+           <Routes />
           </div>
         </div>
       </div>
