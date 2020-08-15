fe5f1f28213b43d2974fdc4d1608fe69652f2c49
Mykola Rudnev <RudnevMykola@gmail.com>
2020-08-15 21:11:58 +0200

    Custom hook and Background

--- moving-box-link/src/App.js
+++ moving-box-link/src/App.js
@@ -1,19 +1,20 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import "./App.css";
+import useMovement from "./useMovement";
 
 export default function App() {
   const canvasRef = useRef(null);
-  const linkDownRef = useRef(null); 
-  const linkUpRef = useRef(null); 
-  const linkRightRef = useRef(null); 
-  const linkLeftRef = useRef(null); 
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-  const [direction, setDirection] = useState('down');
+  const linkDownRef = useRef(null);
+  const linkUpRef = useRef(null);
+  const linkRightRef = useRef(null);
+  const linkLeftRef = useRef(null);
+  const { x, y, direction, move } = useMovement();
 
   //set the height and width of canvas
   useEffect(() => {
- 
+    const context = canvasRef.current.getContext("2d");
+    context.canvas.height = window.innerHeight;
+    context.canvas.width = window.innerWidth;
   }, []);
 
   //move the cox if x or changes
@@ -21,36 +22,14 @@ export default function App() {
     const context = canvasRef.current.getContext("2d");
     context.clearRect(0, 0, window.innerHeight, window.innerWidth);
 
-    let theLinkRef
-    if(direction === 'down') theLinkRef = linkDownRef;
-    if(direction === 'up') theLinkRef = linkUpRef;
-    if(direction === 'left') theLinkRef = linkLeftRef;
-    if(direction === 'right') theLinkRef = linkRightRef;
+    let theLinkRef;
+    if (direction === "down") theLinkRef = linkDownRef;
+    if (direction === "up") theLinkRef = linkUpRef;
+    if (direction === "left") theLinkRef = linkLeftRef;
+    if (direction === "right") theLinkRef = linkRightRef;
 
     context.drawImage(theLinkRef.current, x, y);
   }, [x, y]);
-
-  //add event listener to the window to listener  for arrow kyes
-  useEffect(() => {
-    window.addEventListener("keydown", handleKeyDown);
-
-    function handleKeyDown(e) {
-      if (e.key === "ArrowUp") move("up");
-      if (e.key === "ArrowLeft") move("left");
-      if (e.key === "ArrowDown") move("down");
-      if (e.key === "ArrowRight") move("right");
-    }
-
-    return () => window.removeEventListener("keydown", handleKeyDown);
-  }, []);
-
-  function move(dir) {
-    setDirection(dir);
-    if (dir === "up")   setY((y) => y - 25);
-    if (dir === "left") setX((x) => x - 25);
-    if (dir === "down") setY((y) => y + 25);
-    if (dir === "right")setX((x) => x + 25);
-  }
 
   return (
     <div className="app">
@@ -64,21 +43,22 @@ export default function App() {
       </div>
 
       <div className="images">
-        <img ref={linkDownRef} 
-        src="https://i.imgur.com/JYUB0m3.png" 
-        alt="Down" />
-        <img 
-        ref={linkRightRef} 
-        src="https://i.imgur.com/GEXD7bk.gif" 
-        alt="Right" />
-        <img 
-        ref={linkUpRef} 
-        src="https://i.imgur.com/XSA2Oom.gif" 
-        alt="Up" />
-        <img 
-        ref={linkLeftRef} 
-        src="https://i.imgur.com/4LGAZ8t.gif" 
-        alt="Left" />
+        <img
+          ref={linkDownRef}
+          src="https://i.imgur.com/JYUB0m3.png"
+          alt="Down"
+        />
+        <img
+          ref={linkRightRef}
+          src="https://i.imgur.com/GEXD7bk.gif"
+          alt="Right"
+        />
+        <img ref={linkUpRef} src="https://i.imgur.com/XSA2Oom.gif" alt="Up" />
+        <img
+          ref={linkLeftRef}
+          src="https://i.imgur.com/4LGAZ8t.gif"
+          alt="Left"
+        />
       </div>
     </div>
   );
